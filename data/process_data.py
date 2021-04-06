1d6af024d6f92b8c0a4991ace1c9478d611f700e
v19msheikh <79280358+v19msheikh@users.noreply.github.com>
2021-04-06 20:45:31 +0200

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -13,71 +13,86 @@ Arguments:
 """
 
 import sys
-import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
- 
-def load_data(messages_filepath, categories_filepath):
-    """
-    Load Data function
-    
-    Arguments:
-        messages_filepath -> path to messages csv file
-        categories_filepath -> path to categories csv file
-    Output:
-        df -> Loaded dasa as Pandas DataFrame
-    """
-    messages = pd.read_csv(messages_filepath)
-    categories = pd.read_csv(categories_filepath)
-    df = pd.merge(messages,categories,on='id')
-    return df 
 
-def clean_data(df):
-    """
-    Clean Data function
-    
-    Arguments:
-        df -> raw data Pandas DataFrame
-    Outputs:
-        df -> clean data Pandas DataFrame
-    """
-    categories = df.categories.str.split(pat=';',expand=True)
-    firstrow = categories.iloc[0,:]
-    category_colnames = firstrow.apply(lambda x:x[:-2])
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+    INPUT:
+        'message_filepath' : a string/path to a csv file
+        'categories_filepath' : a string/path to a csv file
+    OUTPUT:
+        'df' : a loaded/transformed pandas dataframe
+    '''
+
+    messages = pd.read_csv(messages_filepath)  # load messages data
+    categories = pd.read_csv(categories_filepath)  # load categories data
+
+    # merge the datasets
+    df = pd.merge(messages, categories, on='id')
+
+    # split the categories into separate category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories[:1]
+
+    # extract numbers from columns
+    category_colnames = row.apply(lambda x: x[0][:-2])
+
+    # rename the columns of `categories`
     categories.columns = category_colnames
+
     for column in categories:
-        categories[column] = categories[column].str[-1]
-        categories[column] = categories[column].astype(np.int)
-    df = df.drop('categories',axis=1)
-    df = pd.concat([df,categories],axis=1)
-    #df = pd.concat([df,pd.get_dummies(df.genre)],axis=1)
-    #df = df.drop(['genre','social'],axis=1) 
-    df = df.drop_duplicates()
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.slice(-1)
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # drop the original categories column from `df`
+    df = df.drop('categories', axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
     return df
 
+
+def clean_data(df):
+    '''
+    INPUT:
+        - pandas dataframe
+    OUTPUT:
+        - pandas dataframe with duplicates removed
+    '''
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    # remove 'original' column with high number of NaNs
+    df = df.drop('original', axis=1)
+
+    return df
+
+
 def save_data(df, database_filename):
-    """
-    Save Data function
-    
-    Arguments:
-        df -> Clean data Pandas DataFrame
-        database_filename -> database file (.db) destination path
-    """
-    engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('df', engine, index=False)
-    pass  
+    '''
+    INPUT:
+        - cleaned dataframe ready to move to sql
+        - string of desired database name, in format 'databasename.db'
+    OUTPUT:
+        - None
+    '''
+
+    # create sqlite engine
+    engine = create_engine('sqlite:///' + str(database_filename))
+
+    # send df to sqlite file, omitting the index
+    df.to_sql('disaster_data', engine, index=False)
 
 
 def main():
-    """
-    Main Data Processing function
-    
-    This function implement the ETL pipeline:
-        1) Data extraction from .csv
-        2) Data cleaning and pre-processing
-        3) Data loading to SQLite database
-    """
-    print(sys.argv)
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
@@ -88,18 +103,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. Example: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
