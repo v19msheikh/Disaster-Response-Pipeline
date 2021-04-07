59c8820bd53aa85be85d7a5ac6c603b26b860ad6
v19msheikh <79280358+v19msheikh@users.noreply.github.com>
2021-04-07 08:57:44 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -3,11 +3,7 @@ import plotly
 import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk import pos_tag, word_tokenize
-import nltk
-
-from sklearn.base import BaseEstimator, TransformerMixin
+from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -17,24 +13,6 @@ from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
-
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -49,7 +27,7 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('df', engine)
+df = pd.read_sql_table('DisasterMessages', engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
@@ -61,18 +39,14 @@ model = joblib.load("../models/classifier.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
-    category_names = df.iloc[:,4:].columns
-    category_boolean = (df.iloc[:,4:] != 0).sum().values
-    
-    
+    # Top five categories count
+    top_category_count = df.iloc[:,4:].sum().sort_values(ascending=False)[1:6]
+    top_category_names = list(top_category_count.index)
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
-            # GRAPH 1 - genre graph
         {
             'data': [
                 Bar(
@@ -91,23 +65,22 @@ def index():
                 }
             }
         },
-            # GRAPH 2 - category graph    
+
         {
             'data': [
                 Bar(
-                    x=category_names,
-                    y=category_boolean
+                    x=top_category_names,
+                    y=top_category_count
                 )
             ],
 
             'layout': {
-                'title': 'Distribution of Message Categories',
+                'title': 'Top Five Categories',
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
-                    'title': "Category",
-                    'tickangle': 35
+                    'title': "Categories"
                 }
             }
         }
